c64db546d479b652c4cccd40651d56fab820e87e
jozzua <jozzu@gmail.com>
2015-05-23 09:54:50 +0800

    Install and setup assemble.io

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
       },
       page: {
         files: {
-          'dist/': ['app/content/page/*.hbs']
+          'dist/': ['app/content/pages/*.hbs']
         }
       },
       blog: {

1d3b39a459ecbda18a9a114b94922404ee9b42fb
jozzua <jozzu@gmail.com>
2015-05-23 09:17:44 +0800

    add gruntfile config for creating pages from handlebars

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,59 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg:grunt.file.readJSON('package.json')
+    watch :{
+      assemble: {
+                  files: [ 'app/content/blog/*.hbs', 
+                           'app/content/pages/*.hbs',
+                           'app/layout/default/*.hbs',
+                           'app/partials/*.hbs'
+                           ],
+                  tasks:['assemble']
+                },
+       livereload {
+        options {
+          livereload:'<%= connect.options.livereload %>'
+        },
+        files: [
+          './dist/*.html'
+        ]
+       },
+    },
+
+    assemble: {
+      options: {
+        layoutdir: 'app/layouts',
+        flatten: true,
+        layout: 'default.hbs'
+        partials: 'app/partials/*.hbs'
+      },
+      page: {
+        files: {
+          'dist/': ['app/content/page/*.hbs']
+        }
+      },
+      blog {
+        files: {
+          'dist/': ['app/content/blog/*.hbs'];
+        }
+      }
+    },
+
+    connect: {
+      options: {
+        port: 8800,
+        hostname: 'localhost',
+        livereload: 35728
+        },
+      livereload: {
+        options: {
+          open: true,
+          base: './dist'
+        }
+      }
+    }
+
+
   });
 
   grunt.loadNpmTasks('assemble');

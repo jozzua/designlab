e0d8a2bf4dbd228dfd7c562a0640438993707803
jozzua <jozzu@gmail.com>
2015-06-29 06:07:39 +0800

    Gulp to create JSON file of contents

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 var gulp = require('gulp');
 var wrap = require('gulp-wrap');
+var directoryMap = require("gulp-directory-map");
+
 
 gulp.task('layout', function () {
   return gulp.src(['app/**/*.html', '!app/layout.html'])
   .pipe(wrap({src: 'app/layout.html'}))
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('directory', function () {
+  gulp.src(['app/**/*.html','!app/layout.html'])
+  .pipe(directoryMap({
+    filename: 'urls.json'
+  }))
   .pipe(gulp.dest('dist'));
 
 });

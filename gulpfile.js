9b59d860a3ee46f7452a2e1a378a46bd2f04400e
jozzua <jozzu@gmail.com>
2015-06-30 07:07:19 +0800

    Create new index, reorganize files

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 var gulp = require('gulp');
 var wrap = require('gulp-wrap');
 var directoryMap = require("gulp-directory-map");
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
 
 
+
+//Add to layout
 gulp.task('layout', function () {
   return gulp.src(['app/**/*.html', '!app/layout.html'])
   .pipe(wrap({src: 'app/layout.html'}))
   .pipe(gulp.dest('dist'));
 });
+
+//Make Directory Listing
 
 gulp.task('directory', function () {
   gulp.src(['app/**/*.html','!app/layout.html'])
@@ -17,3 +23,16 @@ gulp.task('directory', function () {
   .pipe(gulp.dest('dist'));
 
 });
+
+
+// Static server.
+gulp.task('browser-sync', function() {  
+  browserSync({
+    server: {
+      baseDir: './'
+    }
+  });
+  gulp.watch("*.html").on("change", browserSync.reload);
+});
+
+gulp.task('default', ['browser-sync','directory']); 
